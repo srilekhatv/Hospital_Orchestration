e5de059955d9311a24e0bb0b6c4f7457daf25b38
Srilekha Tirumala Vinjamoori <51709857+srilekhatv@users.noreply.github.com>
2025-08-26 00:40:28 -0500

    Update daily_stage_loader.py

--- dags/daily_stage_loader.py
+++ dags/daily_stage_loader.py
@@ -87,7 +87,7 @@ with DAG(
                 '{{ ds }}'::DATE AS execution_date,
                 METADATA$FILENAME AS source_file,
                 COUNT(*) AS rows_loaded
-            FROM @RAW.HOSPITAL_STAGE/{{ (execution_date + macros.timedelta(days=1)).strftime('%Y-%m-%d') }}.csv
+            FROM @RAW.HOSPITAL_STAGE/{{ (execution_date - macros.timedelta(days=1)).strftime('%Y-%m-%d') }}.csv
             (FILE_FORMAT => RAW.CLEANED_CSV_FORMAT)
             GROUP BY METADATA$FILENAME;
         """
